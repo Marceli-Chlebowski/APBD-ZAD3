0da1762d1907f19f76cbc030550e37b3a3a8049f
Marceli <129780388+Marceli-Chlebowski@users.noreply.github.com>
2024-03-26 14:33:11 +0100

    n

---
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -0,0 +1,14 @@
+namespace LegacyApp.Tests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
+    {
+        //arrange
+        var userService = new UserService();
+        //act 
+
+        //assert
+    }
+}
